ec03782e0cb4e62161dc82ae5928c905e9365278
Jeff Lichtman <@zipcar.com>
2018-03-11 14:04:29 -0700

    added programattic tests

--- projects/lichtmaj/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/lichtmaj/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,5 +1,6 @@
 
 
+import com.oracle.tools.packager.Log;
 import junit.framework.TestCase;
 
 import java.util.ArrayList;
@@ -25,9 +26,9 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-      UrlValidator urlValidator = new UrlValidator();
+      UrlValidator urlValidator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
       int validUrlCounter = 0;
-      List<String> validUrls = Arrays.asList("http://www.oregonstate.edu", "www.oregonstate.edu");
+      List<String> validUrls = Arrays.asList("http://www.oregonstate.edu", "http://www.google.com");
       for(String url : validUrls) {
          if(urlValidator.isValid(url) == true){
             System.out.println(url + " Is valid");
@@ -54,12 +55,75 @@ public class UrlValidatorTest extends TestCase {
    }
    //You need to create more test cases for your Partitions if you need to 
    
-   public void testIsValid()
-   {
-	   //You can use this function for programming based testing
+   public void testIsValid() {
+      boolean noFailures = true;
+      UrlValidator urlValidator = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+      for(String scheme : testSchemes) {
+         for(String authority : testAuthorities) {
+            for(String port : testPorts) {
+               for(String path : testPaths) {
+                  for(String query : testQueries) {
+                     String url = scheme + authority + port + path + query;
+                     boolean isValidUrl;
+                     boolean validatorResult = false;
+                     isValidUrl = validSchemes.contains(scheme) && validAuthorities.contains(authority) && validPorts.contains(port)
+                             && validPaths.contains(path) && validQueries.contains(query);
+                     try {
+                        validatorResult = urlValidator.isValid(url);
+                     }
+                     catch (Error e) {
+                        Log.debug("Url validator failure with exception: " + e);
+                     }
+                     if (isValidUrl != validatorResult) {
+                        noFailures = false;
+                        if (isValidUrl) {
+                           System.out.println(url + " is valid but tested as invalid");
+                        }
+                        else {
+                           String invalidText = findInvalidPart(scheme, authority, port, path, query);
+                           System.out.println(url + " is not valid but tested as valid." + invalidText);
+                        }
 
+                     }
+                  }
+               }
+            }
+         }
+      }
+      assertEquals(true, noFailures);
    }
-   
+
+   String findInvalidPart(String scheme, String authority, String port, String path, String query) {
+      String text = " Invalid Parts: (";
+      if(!validSchemes.contains(scheme)) {
+         text = text + "scheme: " + scheme + ", ";
+      }
+      if(!validAuthorities.contains(authority)) {
+         text = text + "authority: " + authority + ", ";
+      }
+      if(!validPorts.contains(port)) {
+         text = text + "port: " + port + ", ";
+      }
+      if(!validPaths.contains(path)) {
+         text = text + "path: " + path + ", ";
+      }
+      if(!validQueries.contains(query)) {
+         text = text + "query: " + query;
+      }
+      text = text + ")";
+      return text;
+   }
+
+   private List<String> validSchemes = Arrays.asList("http://", "https://", "ftp://", "");
+   private List<String> testSchemes = Arrays.asList("http://", "https://", "ftp://", "", "abc", "test://", "http:/");
+   private List<String> validAuthorities = Arrays.asList("www.cool.com", "cool.com", "cool.edu", "www.cool.org");
+   private List<String> testAuthorities = Arrays.asList("www.cool.com", "cool.com", "cool.edu", "www.cool.org", "cool", "", "cool.");
+   private List<String> validPorts = Arrays.asList(":8080", ":0", "", ":35656");
+   private List<String> testPorts = Arrays.asList("", ":8080", ":0", ":35656", ":-8080", ":abc", ":78010");
+   private List<String> validPaths = Arrays.asList("/image", "", "/image/image", "/1test");
+   private List<String> testPaths = Arrays.asList("/image", "", "/image/image", "/1test", "/#", "/&#", "/#&*");
+   private List<String> validQueries = Arrays.asList("?test=true", "", "?test=true&test1=false", "?cool=5");
+   private List<String> testQueries = Arrays.asList("?test=true", "", "?test=true&test1=false", "?cool=5", "#", "?test#5", "###");
 
 
 }
